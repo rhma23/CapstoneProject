b73c8b28cd535911d6f46892672a5f40cd66ef27
Rahma <rahmawtags@gmail.com>
2024-11-30 17:08:48 +0700

    memperbaiki file string

--- app/src/main/java/com/dicoding/projectcapstone/RetrofitClient.kt
+++ app/src/main/java/com/dicoding/projectcapstone/RetrofitClient.kt
@@ -5,7 +5,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "https://da33-103-47-133-132.ngrok-free.app/api/"
+    private const val BASE_URL = "https://58d6-114-5-211-137.ngrok-free.app/api/"
 
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
