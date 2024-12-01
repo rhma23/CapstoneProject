fd4adbfb94271bf05a9cab210c2072a2edabb812
Rahnia <raniamaulidina25@gmail.com>
2024-12-01 14:56:12 +0700

    test nampilin Gmaps

--- app/src/main/java/com/dicoding/projectcapstone/RetrofitClient.kt
+++ app/src/main/java/com/dicoding/projectcapstone/RetrofitClient.kt
@@ -5,7 +5,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://13.229.216.9:80//api/"
+    private const val BASE_URL = "https://ea74-103-47-133-135.ngrok-free.app/api/"
 
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
