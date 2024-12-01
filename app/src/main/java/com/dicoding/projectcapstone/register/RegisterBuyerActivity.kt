551a254526722760f74cdd395782de68f20f53c4
Rahma <rahmawtags@gmail.com>
2024-12-01 20:05:18 +0700

    menambahkan custom view untuk teks login di layout register

--- app/src/main/java/com/dicoding/projectcapstone/register/RegisterBuyerActivity.kt
+++ app/src/main/java/com/dicoding/projectcapstone/register/RegisterBuyerActivity.kt
@@ -18,7 +18,6 @@ class RegisterBuyerActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBuyerBinding
     private lateinit var repository: AuthRepository
     private lateinit var sessionManager: SessionManager
-    private lateinit var buttonRegist: ButtonRegist
 
     private val registerModel: RegisterModel by viewModels {
         RegisterModelFactory(repository)
@@ -31,8 +30,6 @@ class RegisterBuyerActivity : AppCompatActivity() {
 
         repository = AuthRepository.getInstance(RetrofitClient.apiService)
         sessionManager = SessionManager(this)
-
-        buttonRegist = binding.btnRegister
 
         setupView()
         setupAction()
@@ -94,71 +91,56 @@ class RegisterBuyerActivity : AppCompatActivity() {
             } else {
                 binding.etConfirmPassword.error = "Password tidak sama"
             }
-        buttonRegist.setOnClickListener {
-            val intentRegister = Intent(this, RegisterBuyerActivity::class.java)
-            val username = binding.etYourName.text.toString()
-            val email = binding.etEmail.text.toString()
-            val password = binding.etPassword.text.toString()
-            val confirmPassword = binding.etConfirmPassword.text.toString()
-            val role = "buyer"
-            if (password == confirmPassword) {
-                if (binding.etEmail.error == null && binding.etPassword.error == null) {
-                    registerModel.register(username, email, password, role) { success ->
-                        if (!isFinishing && !isDestroyed) {
-                            if (success) {
-                                sessionManager.saveEmail(email)
-                                AlertDialog.Builder(this).apply {
-                                    setTitle("Yeah!")
-                                    setMessage("Akun dengan $email sudah jadi nih. Yuk, login dan belajar coding.")
-                                    setPositiveButton("Lanjut") { _, _ ->
-                                        finish()
-                                        val intent = Intent(
-                                            this@RegisterBuyerActivity,
-                                            OtpRegisterActivity::class.java
-                                        )
-                                        startActivity(intent)
+            binding.btnRegister.setOnClickListener {
+                val intentRegister = Intent(this, RegisterBuyerActivity::class.java)
+                val username = binding.etYourName.text.toString()
+                val email = binding.etEmail.text.toString()
+                val password = binding.etPassword.text.toString()
+                val confirmPassword = binding.etConfirmPassword.text.toString()
+                val role = "buyer"
+                if (password == confirmPassword) {
+                    if (binding.etEmail.error == null && binding.etPassword.error == null) {
+                        registerModel.register(username, email, password, role) { success ->
+                            if (!isFinishing && !isDestroyed) {
+                                if (success) {
+                                    sessionManager.saveEmail(email)
+                                    AlertDialog.Builder(this).apply {
+                                        setTitle("Yeah!")
+                                        setMessage("Akun dengan $email sudah jadi nih. Yuk, login dan belajar coding.")
+                                        setPositiveButton("Lanjut") { _, _ ->
+                                            finish()
+                                            val intent = Intent(
+                                                this@RegisterBuyerActivity,
+                                                OtpRegisterActivity::class.java
+                                            )
+                                            startActivity(intent)
+                                        }
+                                        create()
+                                        show()
                                     }
-                                    create()
-                                    show()
-                                }
-                            } else {
-                                AlertDialog.Builder(this).apply {
-                                    setTitle("Oops!")
-                                    setMessage("Akun dengan $email gagal dibuat. Coba lagi ya.")
-                                    setPositiveButton("Ulangi") { _, _ ->
-                                        startActivity(intentRegister)
-                                        finish()
+                                } else {
+                                    AlertDialog.Builder(this).apply {
+                                        setTitle("Oops!")
+                                        setMessage("Akun dengan $email gagal dibuat. Coba lagi ya.")
+                                        setPositiveButton("Ulangi") { _, _ ->
+                                            startActivity(intentRegister)
+                                            finish()
+                                        }
+                                        create()
+                                        show()
                                     }
-                                    create()
-                                    show()
                                 }
                             }
                         }
                     }
+                } else {
+                    binding.etConfirmPassword.error = "Password tidak sama"
                 }
-            } else {
-                binding.etConfirmPassword.error = "Password tidak sama"
             }
         }
     }
 
     private fun setupEmailValidation() {
-        binding.etEmail.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                if (s != null && !Patterns.EMAIL_ADDRESS.matcher(s).matches()) {
-                    binding.etEmail.error = "Invalid email format"
-                } else {
-                    binding.etEmail.error = null
-                }
-            }
-
-            override fun afterTextChanged(s: Editable?) {}
-        })
-    }
-
-    private fun setupPasswordValidation() {
         binding.etPassword.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
@@ -167,6 +149,22 @@ class RegisterBuyerActivity : AppCompatActivity() {
                     binding.etPassword.error = "Password cannot be less than 8 characters"
                 } else {
                     binding.etPassword.error = null
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+    }
+
+    private fun setupPasswordValidation() {
+        binding.etEmail.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s != null && !Patterns.EMAIL_ADDRESS.matcher(s).matches()) {
+                    binding.etEmail.error = "Invalid email format"
+                } else {
+                    binding.etEmail.error = null
                 }
             }
 
